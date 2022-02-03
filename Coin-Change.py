95946e7f29659d36390a14608582df5390abecef
Niharika Rajaram <abhinihar@gmail.com>
2022-02-02 21:54:46 -0600

    Update Coin-Change.py

--- Coin-Change.py
+++ Coin-Change.py
@@ -12,3 +12,25 @@ class Solution:
             return dp[amount]
         else:
             return -1
+
+# Code 2
+        
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        if coins == None or len(coins) == 0:
+            return 0
+        
+        dp = [[None for i in range(amount+1)] for j in range(len(coins) + 1)]
+        dp[0][0] = 0
+        
+        for i in range(1,amount+1):
+            dp[0][i] = amount + 1
+            
+        for i in range(1,len(coins) + 1):
+            for j in range(0,amount+1):
+                if j< coins[i-1]:
+                    dp[i][j] = dp[i-1][j]
+                else:
+                    dp[i][j] = min(dp[i-1][j],(dp[i][j-coins[i-1]] + 1))
+        
+        return -1 if dp[len(coins)][amount] == amount + 1 else dp[len(coins)][amount]
